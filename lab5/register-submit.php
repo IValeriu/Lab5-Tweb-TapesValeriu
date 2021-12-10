d900a7f4ec6c0e567cb258042c4f76c91f682350
IValeriu <71981439+IValeriu@users.noreply.github.com>
2021-12-10 19:20:15 +0200

    Add files via upload

---
+++ lab5/register-submit.php
@@ -0,0 +1,43 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $rpassword = $_POST['rpassword'];
+    $agree = filter_input(INPUT_POST, 'agree', FILTER_SANITIZE_STRING);
+
+    $errorEmpty = false;
+    $errorEmail = false;
+    $errorPassword = false;
+    $errorRpassword = false;
+    $errorAgree = false;
+
+    $number = preg_match('@[0-9]@', $password);
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
+
+    if (empty($name) || empty($email) || empty($password) || empty($rpassword) || empty($agree)){
+        echo "<span class='form-error'>Fill in all fields!</span>";
+        $errorEmpty = true;
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<span class='form-error'>Write a valid e-mail adress!</span>";
+        $errorEmail = true;
+    }
+    elseif (strlen($password) < 5 || !$number || !$uppercase || !$lowercase || !$specialChars) {
+        echo "<span class='form-error'>Password must be at least 5 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.</span>";
+    }
+    elseif ($rpassword !== $password) {
+        echo "<span class='form-error'>Confirm password is not equal to password!</span>";
+    }
+    else {
+        echo "<span class='form-success'>Form was submited successfully!</span>";
+    }
+}
+else {
+    echo "There was an error!";
+}
+
+?>
